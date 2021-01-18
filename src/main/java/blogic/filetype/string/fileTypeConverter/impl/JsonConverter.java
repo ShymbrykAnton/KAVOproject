e871ff4c0f526c6b26571f41694050dda74cff53
Bohomolov <73234562+Bohomolov@users.noreply.github.com>
2021-01-18 11:51:56 +0200

    Vlad dev (#7)
    * pom.xml, converters + person

--- src/main/java/blogic/filetype/string/fileTypeConverter/impl/JsonConverter.java
+++ src/main/java/blogic/filetype/string/fileTypeConverter/impl/JsonConverter.java
@@ -1,5 +1,29 @@
 package blogic.filetype.string.fileTypeConverter.impl;
 
-public class JsonConverter {
-    //см интерфейс
+import blogic.filetype.string.fileTypeConverter.IFileTypeConverter;
+import blogic.model.Person;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class JsonConverter implements IFileTypeConverter {
+
+    private final Gson json;
+
+    public JsonConverter() {
+        json = new Gson();
+    }
+
+    @Override
+    public String getStrFromPersons(List<Person> persons) {
+        return json.toJson(persons);
+    }
+
+    @Override
+    public List<Person> getPersonsFromString(String strPersons) {
+        Type collectionType = new TypeToken<List<Person>>() {}.getType();
+        return json.fromJson(strPersons, collectionType);
+    }
 }
