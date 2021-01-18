60f2aab378aee331ad19979d7400b2153fe04b5e
Olya <spasitelevaoya@gmail.com>
2021-01-18 14:53:31 +0200

    fhelp

--- src/main/java/util/io/FileHelper.java
+++ src/main/java/util/io/FileHelper.java
@@ -1,7 +1,32 @@
 package util.io;
 
+import java.io.*;
+
 public class FileHelper {
+    public void saveToFile(String content, String fileName) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, false));
+        writer.write(content);
+        writer.close();
+
+    }
+
+    public String readFromFile(String fileName) throws IOException {
+        StringBuilder outputString = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                outputString.append(line);
+
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return outputString.toString();
+    }
+
+}
     //запись и чтение в файл и из файла FileWriter и FileReader
     //проверка на существование файла (метод exists класса File по имени файла)
     //проверка на то пустой ли файл по его длине
-}
