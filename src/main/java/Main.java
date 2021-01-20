d3fecb3fe85560210d57772b44458e094c2004b4
Igilson <coiseamross@gmail.com>
2021-01-21 00:16:46 +0200

    Added Clear All, Exit, and tab - dbs, files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ import gui.view.MainMenu;
 public class Main {
     public static void main(String[] args) {
         MainMenu mainMenu = new MainMenu();
-//        MainMenu.TableSelectionTest tableSelectionTest = new MainMenu.TableSelectionTest();
     }
 }
