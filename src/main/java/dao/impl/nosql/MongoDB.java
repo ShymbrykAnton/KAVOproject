aceb90e4280ce4dba8dce148e913d068bb7ec461
Shymbryk <shymbryk@gmail.com>
2021-02-05 21:32:31 +0200

    Mongo

--- src/main/java/dao/impl/nosql/MongoDB.java
+++ src/main/java/dao/impl/nosql/MongoDB.java
@@ -1,4 +1,78 @@
 package dao.impl.nosql;
 
-public class MongoDB {
+import blogic.model.Person;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import dao.IDatabaseController;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
+
+public class MongoDB implements IDatabaseController {
+
+    private MongoClient getMongoClient() {
+        return new MongoClient("localhost", 27017);
+    }
+
+    private MongoCollection<Document> getCollection() {
+        return getMongoClient().getDatabase("mongodb").getCollection("persons");
+    }
+
+    @Override
+    public void addToDatabase(Person person) {
+        Document document = new Document();
+//        document.put("_id", person.getId());
+        document.put("id", person.getId());
+        document.put("fname", person.getFName());
+        document.put("lname", person.getLName());
+        document.put("age", person.getAge());
+        document.put("city", person.getCity());
+        getCollection().insertOne(document);
+        getMongoClient().close();
+    }
+
+    @Override
+    public List<Person> readFromDatabase() {
+       MongoCursor<Document> documentFindIterable = getCollection().find().iterator();
+       List<Person> personList = new ArrayList<>();
+        Person person;
+        while (documentFindIterable.hasNext()) {
+            person = new Person();
+            Document document = documentFindIterable.next();
+            person.setId(document.getLong("id"));
+            person.setFName(document.getString("fname"));
+            person.setLName(document.getString("lname"));
+            person.setAge((byte) document.getInteger("age",0));
+            person.setCity( document.getString("city"));
+            personList.add(person);
+        }
+        getMongoClient().close();
+        return personList;
+    }
+
+    @Override
+    public void updateDataInPerson(long id, String[] newValue) {
+// Bson filter = eq("student_id", 10000);
+//            Bson updateOperation = set("comment", "You should learn MongoDB!");
+//            UpdateResult updateResult = gradesCollection.updateOne(filter, updateOperation);
+        Bson filter = eq("id", id);
+        Bson updateOperation = and(set("fname", newValue[1]),set("lname",newValue[2]),
+                set("age",Integer.parseInt(newValue[3])),set("city",newValue[4]));
+        getCollection().updateOne(filter, updateOperation);
+        getMongoClient().close();
+    }
+
+    @Override
+    public void removePersonsFromList(long id) {
+        Bson filter = eq("id", id);
+        getCollection().deleteOne(filter);
+        getMongoClient().close();
+    }
 }
